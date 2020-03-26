92a48ff652063115359fe0d23ce9e27c23ab26bf
cngumbi <cngumbi35@gmail.com>
2020-03-26 10:38:09 +0300

    new commit

---
+++ machine/webS.py
@@ -0,0 +1,4 @@
+"""in this project we are going to test the web scraping
+using the beautifulsoup libraries and selenium
+if you dont have this libraries installed please install them 
+open you anaconda environment and lets start"""
